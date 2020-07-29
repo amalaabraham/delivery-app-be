c358723ef0961307570c23b8dc89a3b688f6911a
arihant-2310 <arihant2310@gmail.com>
2020-07-29 12:26:43 +0530

    adds logger to display the running port

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Food Delivery System');
   const app = await NestFactory.create(AppModule);
-  
+
   const options = new DocumentBuilder()
-  .setTitle('Delivery app')
-  .setDescription('The delivery app API description')
-  .setVersion('1.0')
-  .addTag('delivery')
-  .build();
-const document = SwaggerModule.createDocument(app, options);
-SwaggerModule.setup('doc', app, document);
+    .setTitle('Delivery app')
+    .setDescription('The delivery app API description')
+    .setVersion('1.0')
+    .addTag('delivery')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(3000);
+  logger.log(`Application Listening on Port ${3000} `);
+  logger.log(`Api documentation avaliable at "/doc/`);
 }
 bootstrap();

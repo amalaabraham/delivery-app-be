5b1ef1edc3650d3083bb9c95b6dcb8706f4bc7c8
Abhiram R Pai <abhirampai1999@gmail.com>
2020-08-15 19:55:37 +0530

    ADDED REDOC

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import {RedocModule, RedocOptions} from 'nestjs-redoc';
 
 async function bootstrap() {
   const logger = new Logger('Food Delivery System');
@@ -20,9 +21,24 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
+
+  const redocOptions: RedocOptions = {
+    title: 'Hello Nest',
+    logo: {
+      url: 'https://redocly.github.io/redoc/petstore-logo.png',
+      backgroundColor: '#F0F0F0',
+      altText: 'PetStore logo'
+    },
+    sortPropsAlphabetically: true,
+    hideDownloadButton: false,
+    hideHostname: false
+  };
+  // Instead of using SwaggerModule.setup() you call this module
+  await RedocModule.setup('/docs', app, document, redocOptions);
+
   SwaggerModule.setup('doc', app, document);
-  await app.listen(3000);
-  logger.log(`Application Listening on Port ${3000} `);
+  await app.listen(3001);
+  logger.log(`Application Listening on Port ${3001} `);
   logger.log(`Api documentation avaliable at "/doc/`);
 }
 bootstrap();

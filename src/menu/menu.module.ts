fb10cb6a7018042f26ea0431f9097e6a9f49e02d
Abhiram R Pai <abhirampai1999@gmail.com>
2020-08-16 12:23:13 +0530

    fixes

--- src/menu/menu.module.ts
+++ src/menu/menu.module.ts
@@ -6,12 +6,16 @@ import { UserRepository } from 'src/auth/user.repository';
 import { RestaurantRepository } from 'src/restaurant/restaurant.repository';
 import { Menu } from './entities/Menu.entity';
 import { MenuRepository } from './menu.repository';
+import { Restaurant } from 'src/restaurant/entities/Restaurant.entity';
+import { RestaurantService } from 'src/restaurant/restaurant.service';
+import { RestaurantModule } from 'src/restaurant/restaurant.module';
 
 @Module({
   imports:[
     TypeOrmModule.forFeature([Menu,MenuRepository]),
     TypeOrmModule.forFeature([RestaurantRepository]),
     TypeOrmModule.forFeature([UserRepository]),
+    RestaurantModule
   ],
   providers: [MenuService],
   controllers: [MenuController],

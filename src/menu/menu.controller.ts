becb971e3c6476a9f6bf23a307194bf143f3ed15
Jishnu P Sreekumar <psjishnu13@gmail.com>
2020-08-28 16:36:29 +0530

    Updated delete dish

--- src/menu/menu.controller.ts
+++ src/menu/menu.controller.ts
@@ -1,5 +1,18 @@
-import { Controller, Logger, Get, Param, ParseIntPipe, Post, Body, Req, Put, UseGuards, Delete, Patch } from '@nestjs/common';
-import {ApiBearerAuth, ApiTags} from '@nestjs/swagger';
+import {
+  Controller,
+  Logger,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Body,
+  Req,
+  Put,
+  UseGuards,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { MenuService } from './menu.service';
 import { AddMenuDto, UpdateDish, AddDishes } from './dto';
 import { AuthGuard } from '@nestjs/passport';
@@ -9,77 +22,70 @@ import { AddMoreDish } from './dto/addmoredishesdto.dto';
 @ApiTags('Menu Management')
 @Controller('api/v1/menu')
 export class MenuController {
-    private logger =new  Logger('Restaurant Controller');
-    constructor(private readonly menuService: MenuService){}
+  private logger = new Logger('Restaurant Controller');
+  constructor(private readonly menuService: MenuService) {}
 
-    @Get('menu/:resId')
-    getMenuById(@Param('resId') id:string)
-    {
-        this.logger.verbose("retrieving Menu")
-        console.log(id)
-        return this.menuService.getMenuById(id);
-    }
+  @Get('menu/:resId')
+  getMenuById(@Param('resId') id: string) {
+    this.logger.verbose('retrieving Menu');
+    console.log(id);
+    return this.menuService.getMenuById(id);
+  }
 
-    @Get('dishes/:menuId')
-    getDishesByMenuId(@Param('menuId') id:string)
-    {
-        this.logger.verbose('retrieving dishes by menu')
-        return this.menuService.getDishesByMenu(id);
-    }
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @Post('deletedish')
+  deleteDish(@Req() req: any, @Body() deleteDish: DeleteDish) {
+    this.logger.verbose('dish removed');
+    return this.menuService.deleteDish(req.user, deleteDish);
+  }
 
-    @Get('resdishes/:resId')
-    getDishesByRestaurantId(@Param('resId') id:string)
-    {
-        this.logger.verbose('retrieving dishes by restaurant Id')
-        console.log(id);
-        return this.menuService.getDishesByRestaurant(id);
-    }
-    
-    @ApiBearerAuth()
-    @UseGuards(AuthGuard('jwt'))
-    @Post(':resId')
-    addMenu(@Param('resId')id:string,@Body() addMenu:AddMenuDto,@Req() req:any)
-    {
-        this.logger.verbose('adding to menu')
-        return this.menuService.AddMenu(id,addMenu,req.user);
-    }
+  @Get('dishes/:menuId')
+  getDishesByMenuId(@Param('menuId') id: string) {
+    this.logger.verbose('retrieving dishes by menu');
+    return this.menuService.getDishesByMenu(id);
+  }
 
-    @ApiBearerAuth()
-    @UseGuards(AuthGuard('jwt'))
-    @Put('updateDish')
-    updateDish(@Body() updateDish:UpdateDish,@Req() req:any)
-    {
-        this.logger.verbose('updating dish')
-        return this.menuService.UpdateDish(updateDish,req.user);
-    }
+  @Get('resdishes/:resId')
+  getDishesByRestaurantId(@Param('resId') id: string) {
+    this.logger.verbose('retrieving dishes by restaurant Id');
+    console.log(id);
+    return this.menuService.getDishesByRestaurant(id);
+  }
 
-    @ApiBearerAuth()
-    @UseGuards(AuthGuard('jwt'))
-    @Delete('/deletedish')
-    deleteDish(@Req() req:any,@Body() deleteDish:DeleteDish) {
-        this.logger.verbose("dish removed");
-        console.log(deleteDish);
-        return this.menuService.deleteDish(req.user,deleteDish);
-    }
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @Post(':resId')
+  addMenu(
+    @Param('resId') id: string,
+    @Body() addMenu: AddMenuDto,
+    @Req() req: any,
+  ) {
+    this.logger.verbose('adding to menu');
+    return this.menuService.AddMenu(id, addMenu, req.user);
+  }
 
-    @ApiBearerAuth()
-    @UseGuards(AuthGuard('jwt'))
-    @Delete('/:menuid')
-    deleteMenu(@Req() req:any,@Param('menuid')id:string) {
-        this.logger.verbose("menu removed");
-        return this.menuService.deleteMenu(req.user,id);
-    }
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @Put('updateDish')
+  updateDish(@Body() updateDish: UpdateDish, @Req() req: any) {
+    this.logger.verbose('updating dish');
+    return this.menuService.UpdateDish(updateDish, req.user);
+  }
 
-    @ApiBearerAuth()
-    @UseGuards(AuthGuard('jwt'))
-    @Patch('/addmoredish')
-    addmoreDish(@Body() addmoreDish:AddMoreDish,@Req() req:any)
-    {
-        this.logger.verbose('adding dish')
-        return this.menuService.AddMoreDish(addmoreDish,req.user);
-    }
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @Delete('/:menuid')
+  deleteMenu(@Req() req: any, @Param('menuid') id: string) {
+    this.logger.verbose('menu removed');
+    return this.menuService.deleteMenu(req.user, id);
+  }
 
-
-   
-    
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @Patch('/addmoredish')
+  addmoreDish(@Body() addmoreDish: AddMoreDish, @Req() req: any) {
+    this.logger.verbose('adding dish');
+    return this.menuService.AddMoreDish(addmoreDish, req.user);
+  }
 }
--- src/menu/menu.service.ts
+++ src/menu/menu.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MenuRepository } from './menu.repository';
 import { UserRepository } from 'src/auth/user.repository';
@@ -11,234 +16,188 @@ import { DeleteDish } from './dto/deletedishdto.dto';
 import { AddMoreDish } from './dto/addmoredishesdto.dto';
 //import { deleteDish } from './dto/deletedishdto.dto';
 const ObjectId = require('mongodb').ObjectID;
-import { ObjectID } from "mongodb";
+import { ObjectID } from 'mongodb';
 
 @Injectable()
 export class MenuService {
-    constructor(
-        @InjectRepository(MenuRepository)
-        @InjectRepository(UserRepository)
-        @InjectRepository(RestaurantRepository)
-        private readonly menuRepository:MenuRepository,
-        private readonly userRepository:UserRepository,
-        private readonly restaurantRepository:RestaurantRepository,
-        
-        private readonly restaurantService:RestaurantService,
-    )
-    {}
+  constructor(
+    @InjectRepository(MenuRepository)
+    @InjectRepository(UserRepository)
+    @InjectRepository(RestaurantRepository)
+    private readonly menuRepository: MenuRepository,
+    private readonly userRepository: UserRepository,
+    private readonly restaurantRepository: RestaurantRepository,
 
-    async getMenuById(id):Promise<any>{
-        const restaurant = await this.restaurantRepository.findOne(ObjectId(id))
-        console.log(restaurant)
-        if(restaurant)
-        {
-            const menuList = await this.menuRepository.find({restaurantId:id,status:'ACTIVE'})
-            console.log(menuList)
-            if(menuList.length > 0)
-            {
-                return {
-                    success:true,
-                    message:'Menu Retrieved',
-                    data:menuList
-                }
-            }
-            else
-            {
-                throw new NotFoundException({message:'Menu For this Restaurant Not Found'})
-            }
-        }
-        else
-        {
-            throw new NotFoundException({message:'Restaurant Not Found'})
-        }
+    private readonly restaurantService: RestaurantService,
+  ) {}
+
+  async getMenuById(id): Promise<any> {
+    const restaurant = await this.restaurantRepository.findOne(ObjectId(id));
+    console.log(restaurant);
+    if (restaurant) {
+      const menuList = await this.menuRepository.find({
+        restaurantId: id,
+        status: 'ACTIVE',
+      });
+      console.log(menuList);
+      if (menuList.length > 0) {
+        return {
+          success: true,
+          message: 'Menu Retrieved',
+          data: menuList,
+        };
+      } else {
+        throw new NotFoundException({
+          message: 'Menu For this Restaurant Not Found',
+        });
+      }
+    } else {
+      throw new NotFoundException({ message: 'Restaurant Not Found' });
     }
+  }
 
-
-    async AddMenu(id,menu:AddMenuDto,user:User):Promise<any>{
-        try{
-            console.log("hi")
-            console.log(user)
-            console.log(id)
-            if(await this.restaurantService.findHotel(user,id))
-            {
-                return this.menuRepository.addMenu(id,menu);
-
-            }
-        }
-        catch(e)
-        {
-            throw new HttpException({message:e},HttpStatus.BAD_REQUEST);
-        }
+  async AddMenu(id, menu: AddMenuDto, user: User): Promise<any> {
+    try {
+      console.log('hi');
+      console.log(user);
+      console.log(id);
+      if (await this.restaurantService.findHotel(user, id)) {
+        return this.menuRepository.addMenu(id, menu);
+      }
+    } catch (e) {
+      throw new HttpException({ message: e }, HttpStatus.BAD_REQUEST);
     }
-    
-    async getDishesByMenu(id):Promise<any>{
-        try{
-            const menu = await this.menuRepository.findOne(ObjectId(id))
-            if(menu.status == "ACTIVE")
-            {
-                return {
-                    success:true,
-                    message:'Dishes Retrieved',
-                    data:menu.dishes
-                }
-            }
-            else{
-                throw new NotFoundException({message:'Menu Not Found'})
-            }
-        }
-        catch(e)
-        {
-            throw new HttpException({message:e},HttpStatus.BAD_REQUEST)
-        }
+  }
+
+  async getDishesByMenu(id): Promise<any> {
+    try {
+      const menu = await this.menuRepository.findOne(ObjectId(id));
+      if (menu.status == 'ACTIVE') {
+        return {
+          success: true,
+          message: 'Dishes Retrieved',
+          data: menu.dishes,
+        };
+      } else {
+        throw new NotFoundException({ message: 'Menu Not Found' });
+      }
+    } catch (e) {
+      throw new HttpException({ message: e }, HttpStatus.BAD_REQUEST);
     }
+  }
 
-    async getDishesByRestaurant(id):Promise<any>{
-        const menu = await this.menuRepository.find({restaurantId:id})
-        var result = [];
-        
-        if(menu.length>0)
-        {
-            for(var i=0;i<menu.length;i++)
-            {
-                result.push(menu[i]);
+  async getDishesByRestaurant(id): Promise<any> {
+    const menu = await this.menuRepository.find({ restaurantId: id });
+    var result = [];
+
+    if (menu.length > 0) {
+      for (var i = 0; i < menu.length; i++) {
+        result.push(menu[i]);
+      }
+      return {
+        success: true,
+        message: 'dishes retrieved',
+        data: result,
+      };
+    } else {
+      throw new NotFoundException({ message: 'Dishes not Found' });
+    }
+  }
+  async UpdateDish(dish: UpdateDish, user: User): Promise<any> {
+    try {
+      console.log('hi');
+      console.log(user);
+      if (await this.restaurantService.findHotel(user, ObjectId(dish.resId))) {
+        const menu = await this.menuRepository.findOne(ObjectId(dish.menuId));
+        //console.log(menu);
+        if (menu) {
+          for (var i = 0; i < menu.dishes.length; i++) {
+            if (menu.dishes[i].dishId == dish.dishId) {
+              console.log('here');
+              break;
             }
-            return {
-                success:true,
-                message:'dishes retrieved',
-                data:result
-            }
+          }
+          if (dish.name) {
+            menu.dishes[i].name = dish.name;
+          }
+          if (dish.price) {
+            menu.dishes[i].price = dish.price;
+          }
+          if (dish.photos) {
+            menu.dishes[i].photo = dish.photos;
+          }
+          await this.menuRepository.save(menu);
         }
-        else{
-            throw new NotFoundException({message:'Dishes not Found'})
+      }
+    } catch {}
+  }
+
+  async AddMoreDish(dish: AddMoreDish, user: User): Promise<any> {
+    try {
+      if (await this.restaurantService.findHotel(user, ObjectId(dish.resId))) {
+        const menu = await this.menuRepository.findOne(ObjectId(dish.menuId));
+        //console.log(menu);
+        if (menu) {
+          dish['dishId'] = new ObjectID();
+          delete dish.menuId;
+          delete dish.resId;
+          menu.dishes.push(dish);
+          await this.menuRepository.save(menu);
         }
-    }
-    async UpdateDish(dish:UpdateDish,user:User):Promise<any>{
-        try{
-            console.log("hi")
-            console.log(user)
-            if(await this.restaurantService.findHotel(user,ObjectId(dish.resId)))
-            {
-                const menu = await this.menuRepository.findOne(ObjectId(dish.menuId));
-                //console.log(menu);
-                if(menu)
-                {
-                    for (var i =0;i<menu.dishes.length;i++)
-                    {
-                        
-                        if(menu.dishes[i].dishId==dish.dishId)
-                        {
-                            console.log("here");
-                            break;
-                        }
-                    }
-                    if(dish.name)
-                    {
-                        menu.dishes[i].name=dish.name;
-                    }
-                    if(dish.price)
-                    {
-                        menu.dishes[i].price=dish.price;
-                    }
-                    if(dish.photos)
-                    {
-                        menu.dishes[i].photo=dish.photos;
-                    }
-                    await this.menuRepository.save(menu);
-                }
-            }
+      }
+    } catch {}
+  }
 
-
-    }
-    catch{}
-    }
-
-    async AddMoreDish(dish:AddMoreDish,user:User):Promise<any>{
-        try{
-        
-            if(await this.restaurantService.findHotel(user,ObjectId(dish.resId)))
-            {
-                const menu = await this.menuRepository.findOne(ObjectId(dish.menuId));
-                //console.log(menu);
-                if(menu)
-                {
-                        dish['dishId']= new ObjectID();
-                        delete dish.menuId;
-                        delete dish.resId;
-                         menu.dishes.push(dish);
-                    await this.menuRepository.save(menu);
-                }
-            }
-
-            
-
-    }
-    catch{}
-            
-
-
-        }
-
-    async deleteMenu(user:User,id):Promise<any> {
-        const menu = await this.menuRepository.findOne(ObjectId(id))
-        if(await this.restaurantService.findHotel(user,menu.restaurantId)){
-        //const restaurant = await this.restaurantRepository.findOne(ObjectId(id))
-        if(menu){
-            menu.status = "NOT_AVAILABLE"
+  async deleteMenu(user: User, id): Promise<any> {
+    const menu = await this.menuRepository.findOne(ObjectId(id));
+    if (await this.restaurantService.findHotel(user, menu.restaurantId)) {
+      //const restaurant = await this.restaurantRepository.findOne(ObjectId(id))
+      if (menu) {
+        menu.status = 'NOT_AVAILABLE';
         await this.menuRepository.save(menu);
-        return{
-            sucess:true,
-            message: 'Deleted Successfully'
-        }
+        return {
+          sucess: true,
+          message: 'Deleted Successfully',
+        };
+      } else {
+        return {
+          sucess: false,
+          message: 'Deletion Failed',
+        };
+      }
     }
-        else{
-            return{
-                sucess:false,
-                message: 'Deletion Failed'
-            }
-        }
-    }
-    
-    
-    }
+  }
 
-    async deleteDish(user:User,dish:DeleteDish):Promise<any> {
-        const menu = await this.menuRepository.findOne(ObjectId(dish.menuId))
-        if(await this.restaurantService.findHotel(user,ObjectId(dish.resId))){
-        //const restaurant = await this.restaurantRepository.findOne(ObjectId(id))
-        if(menu){
-            console.log(menu)
-            console.log(menu.dishes.length)
+  async deleteDish(user: User, dish: DeleteDish): Promise<any> {
+    const menu = await this.menuRepository.findOne(ObjectId(dish.menuId));
+    if (await this.restaurantService.findHotel(user, ObjectId(dish.resId))) {
+      //const restaurant = await this.restaurantRepository.findOne(ObjectId(id))
+      if (menu) {
+        console.log(menu);
+        console.log(menu.dishes.length);
 
-
-            for (var i =0;i<menu.dishes.length;i++)
-                    {
-                       
-                        
-                        if(menu.dishes[i].dishId==dish.dishId)
-                        {
-                            break;
-                        }
-                    }
-                  // await this.menuRepository.delete(menu.dishes[i]);
-                  menu.dishes.splice(i,1)
-                   //delete menu.dishes[i];
-                   await this.menuRepository.save(menu);
-
-                   //const afterdeletemenu = await this.menuRepository.findOne(ObjectId(dish.menuId))
-                   //console.log(afterdeletemenu)
-        return{
-            sucess:true,
-            message: 'Deleted Successfully'
+        for (var i = 0; i < menu.dishes.length; i++) {
+          if (menu.dishes[i].dishId == dish.dishId) {
+            break;
+          }
         }
+        // await this.menuRepository.delete(menu.dishes[i]);
+        menu.dishes.splice(i, 1);
+        //delete menu.dishes[i];
+        await this.menuRepository.save(menu);
+
+        //const afterdeletemenu = await this.menuRepository.findOne(ObjectId(dish.menuId))
+        //console.log(afterdeletemenu)
+        return {
+          sucess: true,
+          message: 'Deleted Successfully',
+        };
+      } else {
+        return {
+          sucess: false,
+          message: 'Deletion Failed',
+        };
+      }
     }
-        else{
-            return{
-                sucess:false,
-                message: 'Deletion Failed'
-            }
-        }
-    }
-    
-    
-    }
+  }
 }
